4d36af1d9aa0b4694ea8adcdfce1047a87a26c87
Nelli Ohanyan <neohanya@c1r1s1.42yerevan.am>
2026-04-09 19:27:57 +0400

    replaced 'comment' to 'rank' in disorder function

--- disorder.c
+++ disorder.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   disorder.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adarmoya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: neohanya <<marvin@42.fr>>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/27 14:58:09 by adarmoya          #+#    #+#             */
-/*   Updated: 2026/04/08 20:37:06 by adarmoya         ###   ########.fr       */
+/*   Updated: 2026/04/09 18:37:24 by neohanya         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ float	disorder(t_stack *st)
 		while (cur2)
 		{
 			total_pairs++;
-			if (cur1->content > cur2->content)
+			if (cur1->rank > cur2->rank)
 				mistakes++;
 			cur2 = cur2->next;
 		}
